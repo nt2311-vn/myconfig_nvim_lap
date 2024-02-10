c89ead7674a57a8fa0fdb249ff33d5bc9613c378
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 23:54:29 +0700

    Add nvim lsp and fix on global nvim error

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,31 +18,16 @@ return {
     config = function()
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({
-				settings = {
+settings = {
     Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
       diagnostics = {
-        -- Get the language server to recognize the `vim` global
         globals = {
           'vim',
-          'require'
         },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
       },
     },
   },
-			})
+})
       lspconfig.tsserver.setup({})
       lspconfig.pyright.setup({})
 
