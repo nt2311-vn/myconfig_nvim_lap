e0afb1bb92c04ac6254efa13220d78dc7e91b797
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 22:29:32 +0700

    Implement nvim-treesitter

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,46 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+  local configs = require("nvim-treesitter.configs")
+      configs.setup({
+          ensure_installed = { 
+            -- low level
+            "c",
+            "cpp",
+
+            -- respective lang
+            "go",
+            "rust",
+            "c_sharp",
+
+            --high level
+            "python",
+            "lua",
+            "javascript",
+            "typescript",
+
+            --web dev
+            "html",
+            "css",
+            "vue",
+            "svelte",
+
+
+            --utility
+            "jq",
+            "dockerfile",
+            "json",
+            "yaml",
+            "gitignore",
+            "jsdoc",
+            "vim",
+            "vimdoc",
+
+          },
+          sync_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+  end
+}
