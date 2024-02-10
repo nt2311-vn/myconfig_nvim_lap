b6829b6ee0db5e938f1cb1a89013d5cb6008d6bb
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 21:42:19 +0700

    Add plugins lua

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.clipboard = "unnamedplus"
+vim.wo.relativenumber = true
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
