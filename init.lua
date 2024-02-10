29085eb0e4843f3a83b987f988b79956431fb790
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 22:37:14 +0700

    Implement keymappings done

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.opt.clipboard = "unnamedplus"
-vim.wo.relativenumber = true
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
