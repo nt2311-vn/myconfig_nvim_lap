afb1a3d6c3303b9729b27aaf8f55251f9500350b
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 21:49:07 +0700

    restructure theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,5 @@
 return {
-  {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  },
+
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
