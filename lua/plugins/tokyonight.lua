b982ede2367a5b82605a9161d19c60b9c7749f44
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 22:11:31 +0700

    Fix break theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,4 +3,17 @@ return {
   lazy = false,
   priority = 1000,
   opts = {},
+  config = function()
+    require("tokyonight").setup {
+      style = "moon",
+      transparent = false,
+      terminal_colors = true,
+      styles = {
+        comments = {italic = true},
+        keywords = {italic = false},
+      },
+      lualine_bold = true
+    }
+    vim.cmd("colorscheme tokyonight-night")
+  end
 }
