f529d3fb352479f875c183d2d57df401dfa34b2a
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 22:33:28 +0700

    Complete implement neotree lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,3 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-},
 }
 
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+    window = {
+          position = "left",
+          width = 30,
+          },
+    })
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+  end
+}
