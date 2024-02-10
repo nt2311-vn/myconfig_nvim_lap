4a046cface808d9da620d7a2377c7fe19b0319ea
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-10 23:01:19 +0700

    Add mason nvim lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,16 @@
 return {
-  "williamboman/mason.nvim"
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "tsserver", "pyright"},
+      })
+    end
+  },
 }
