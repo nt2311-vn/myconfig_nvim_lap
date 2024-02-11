6602c24b7811265d8aed2b3ed0af3cfcca56d36e
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-11 15:59:15 +0700

    Add gitsigns and complete to plugins

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
-  end,
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+	end,
 }
