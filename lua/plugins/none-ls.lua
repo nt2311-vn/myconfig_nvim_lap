e5b7907cd60d7060c7c39d98d6e7920faabeccbd
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-11 07:18:52 +0700

    Add none-ls to format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -14,8 +14,6 @@ return {
 						group = augroup,
 						buffer = bufnr,
 						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
 							vim.lsp.buf.format({ async = false })
 						end,
 					})
