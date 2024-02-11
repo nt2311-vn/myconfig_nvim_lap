6b1fff0d9ec3dc29522460284acb43ae7a1e3d0b
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-11 16:01:30 +0700

    add git signs toggle done

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,5 +3,6 @@ return {
 	config = function()
 		require("gitsigns").setup()
 		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
 	end,
 }
